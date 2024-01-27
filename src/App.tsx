f317a15b0f5f8a191ff7542adf9cb946bd4c8bf1
unknown <amienulrana@gmail.com>
2024-01-27 17:23:20 +0700

    render section navbar and hero

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+import { BrowserRouter } from "react-router-dom";
+
+import {
+  Hero,
+  Navbar,
+} from "./components";
+import { useEffect } from "react";
+import { config } from "./constants/config";
+
+const App = () => {
+  useEffect(() => {
+    if (document.title !== config.html.title) {
+      document.title = config.html.title;
+    }
+  }, []);
+
+  return (
+    <BrowserRouter>
+      <div className="bg-primary relative z-0 overflow-x-hidden">
+        <div className="bg-hero-pattern heroElem w-full bg-cover bg-center bg-no-repeat">
+          <Navbar />
+          <Hero />
+        </div>
+      </div>
+    </BrowserRouter>
+  );
+};
+
+export default App;
