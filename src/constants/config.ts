deefb431a4bcdcb524cfbdc0e6c5324cbf297086
unknown <amienulrana@gmail.com>
2024-01-27 19:14:25 +0700

    create file config for the typhography in all element

---
+++ src/constants/config.ts
@@ -0,0 +1,37 @@
+export const config = {
+  html: {
+    title: "Amienul Rana",
+    fullName: "Amienul Rana",
+    email: "johndoe@mail.com",
+  },
+  contact: {
+    p: "Get in touch",
+    h2: "Contact.",
+    form: {
+      email: { span: "Your Email", placeholder: "What's your email?" },
+      message: {
+        span: "Your Message",
+        placeholder: "What do you want to say?",
+      },
+    },
+  },
+  sections: {
+    about: {
+      p: "Introduction",
+      h2: "Overview.",
+    },
+    experience: {
+      p: "What I have done so far",
+      h2: "Work Experience.",
+    },
+    works: {
+      p: "My work",
+      h2: "Projects.",
+      content: `Following projects showcases my skills and experience through
+    real-world examples of my work. Each project is briefly described with
+    links to code repositories and live demos in it. It reflects my
+    ability to solve complex problems, work with different technologies,
+    and manage projects effectively.`,
+    },
+  },
+};
