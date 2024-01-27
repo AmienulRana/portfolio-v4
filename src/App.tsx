169735e9af1d6dbcde42cf041bdf3755de6a3db7
unknown <amienulrana@gmail.com>
2024-01-27 22:59:53 +0700

    Add stars canvas in hero section

--- src/App.tsx
+++ src/App.tsx
@@ -10,22 +10,30 @@ import {
   Works,
   StarsCanvas,
 } from "./components";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { config } from "./constants/config";
 
 const App = () => {
+  const [showStarHero, setShowStarHero] = useState(false);
   useEffect(() => {
     if (document.title !== config.html.title) {
       document.title = config.html.title;
     }
+
+    setTimeout(() => {
+      setShowStarHero(true)
+    }, 1000)
   }, []);
 
   return (
     <BrowserRouter>
       <div className="bg-primary relative z-0 overflow-x-hidden">
-        <div className="bg-hero-pattern heroElem w-full bg-cover bg-center bg-no-repeat">
+        <div className="bg-hero-pattern relative z-0 heroElem w-full bg-cover bg-center bg-no-repeat">
           <Navbar />
           <Hero />
+          {showStarHero && (
+            <StarsCanvas sphere={900} />
+          )}
         </div>
         <About />
         <Works />
