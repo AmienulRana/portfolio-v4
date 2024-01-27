8c92b033a2447b5e931c0c624bb9837d0678967b
unknown <amienulrana@gmail.com>
2024-01-27 19:08:19 +0700

    export all file in the folder component in one file

---
+++ src/components/index.ts
@@ -0,0 +1,24 @@
+// @ts-nocheck
+import {
+  StarsCanvas,
+} from "./canvas";
+import Hero from "./sections/Hero";
+import Navbar from "./layout/Navbar";
+import About from "./sections/About";
+import Tech from "./sections/Tech";
+import Experience from "./sections/Experience";
+import Works from "./sections/Works";
+import Contact from "./sections/Contact";
+import CanvasLoader from "./layout/Loader";
+
+export {
+  Hero,
+  Navbar,
+  About,
+  Tech,
+  Experience,
+  Works,
+  Contact,
+  CanvasLoader,
+  StarsCanvas,
+};
