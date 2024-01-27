08a724ed61cf8557d46a1c7191058af1d16c429c
unknown <amienulrana@gmail.com>
2024-01-27 18:59:07 +0700

    add image file for show my work

---
+++ src/assets/index.ts
@@ -0,0 +1,44 @@
+import strt from './company/strt.png';
+import armada from './company/armada.png';
+import dilo from './company/dilo.jpeg';
+import aedu from './company/aedu.webp';
+
+import github from './github.png';
+
+import callyuk from './projects/call-yuk.webp';
+import learning from './projects/aedu.webp';
+import hris from './projects/hris.webp';
+import nginep from './projects/nginep.webp';
+import creo from './projects/creo-defi.webp';
+import app_aedu from './projects/mobile-aedu.webp';
+import movie_app from './projects/movie-app.webp';
+import receipe from './projects/receipe.png';
+import invitation from './projects/undangan digital.webp';
+import netflix from './projects/netflix.webp';
+import landing_page from './projects/landing page property.webp';
+import marketplace from './projects/marketplace.webp';
+import online_shop from './projects/hefa store.webp';
+import car_rental from './projects/car-rental-v2.webp';
+import topup_game from './projects/topupgame.webp';
+
+export {
+  strt,
+  dilo,
+  aedu,
+  armada,
+  learning,
+  marketplace,
+  app_aedu,
+  movie_app,
+  creo,
+  hris,
+  netflix,
+  online_shop,
+  nginep,
+  landing_page,
+  car_rental,
+  receipe,
+  invitation,
+  topup_game, callyuk,
+  github
+};
